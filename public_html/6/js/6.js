e6ad1682233aa11e72ef577ff507250e6179a7e3
DANIELA <DANIELA@10.72.133.137>
2017-07-19 16:48:06 -0500

    Finalice el punto 6.

---
+++ public_html/6/js/6.js
@@ -0,0 +1,21 @@
+//Punto 6
+
+var num1 = ("10");
+var num2 = ("50");
+
+var result = num2 ** num1;
+console.log("50 a la 10 es: " + result);
+var result = num1 ** num2;
+console.log("10 a la 50 es: " + result);
+var multiplicacion = num2 * num1;
+console.log("La multiplicacion entre 50 y 10 es: " + multiplicacion);
+var division = num2 / num1;
+console.log("La division entre 50 y 10 es: " + division);
+var division = num1 / num2;
+console.log("La division entre 10 y 50 es: " + division);
+var porcentaje = num2 % num1;
+console.log("El modulo de la division entre 50 y 10 es: " + porcentaje);
+var resta = num2 - num1;
+console.log("La resta entre 50 y 10 es: " + resta);
+var resta = num1 - num2;
+console.log("La resta entre 10 y 50 es: " + resta);
