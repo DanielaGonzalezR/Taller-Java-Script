941f527657aa24b2da002c8490d230d07bce022b
DANIELA <DANIELA@10.72.133.137>
2017-07-19 16:49:00 -0500

    Finalice el punto 8.

---
+++ public_html/8/js/8.js
@@ -0,0 +1,16 @@
+//Punto 8
+
+var num1 = Number(prompt("Enter a number"));
+var num2 = Number(prompt("Enter another number"));
+
+var sum = num1 + num2;
+console.log("The sum of " + num1 + " and " + num2 + " is: " + sum);
+var subtraction = num1 - num2;
+console.log("the sustraction " + num1 + " and " + num2 + " is: " + subtraction);
+var multiplication = num1 * num2;
+console.log("The multiplication of " + num1 + " and " + num2 + " is: " + multiplication);
+var division = num1 / num2;
+console.log("The division of " + num1 + " and " + num2 + " is: " + +division);
+
+
+
