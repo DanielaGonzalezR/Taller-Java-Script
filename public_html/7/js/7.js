ee53a689ff84d4b605530c72de0242b969edb847
DANIELA <DANIELA@10.72.133.137>
2017-07-19 16:48:21 -0500

    Finalice el punto 7.

---
+++ public_html/7/js/7.js
@@ -0,0 +1,17 @@
+//Punto 7
+
+var num1 = Number(prompt("Enter a number"));
+var num2 = Number(prompt("Enter another number"));
+
+var result = num1 ** num2;
+console.log(num1 + " to " + num2 + " is:" + result);
+var result = num2 ** num1;
+console.log(num2 + " to " + num1 + " is:" + result);
+var multiplication = num2 * num1;
+console.log("Multiplication between" + num1 + " and " + num2 + " is: " + multiplication);
+var division = num1 / num2;
+console.log("The division between " + num1 + " and " + num2 + " is: " + division);
+var division = num2 / num1;
+console.log("The division between" + num2 + " and " + num1 + " is: " + division);
+var module = num2 % num1;
+console.log("The module of the division between " + num1 + " and " + num2 + " is: " + module);
