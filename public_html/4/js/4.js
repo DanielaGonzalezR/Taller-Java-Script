654ee87de3a41179727fef37f6d3723ca04fb694
DANIELA <DANIELA@10.72.133.137>
2017-07-19 16:47:35 -0500

    Finalice el punto 4.

---
+++ public_html/4/js/4.js
@@ -0,0 +1,21 @@
+//Punto 4
+
+var name = prompt("Escriba su nombre completo");
+console.log("Hola, mi nombre es " + name);
+var id = prompt("Tipo de documeto");
+var number = prompt("escriba numero de documento");
+console.log("Mi numero de " + id + " es " + number);
+var gender = prompt("escriba su genero");
+console.log("Genero biologico " + gender);
+var height = prompt("Escriba tu estatura");
+console.log("Mi estatura es: " + height + " mts.");
+var live = prompt("¿Es verdad que vive en Cartago?");
+if (live === "si") {
+  console.log("True");
+} else {
+  console.log("False");
+}
+
+
+
+
