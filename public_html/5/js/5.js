a501ec200b2a9e0905641c55ed1612e4700c2537
DANIELA <DANIELA@10.72.133.137>
2017-07-19 16:47:53 -0500

    Finalice el punto 5.

---
+++ public_html/5/js/5.js
@@ -0,0 +1,8 @@
+//Punto 5
+
+var name = prompt("Escriba su nombre completo");
+console.log("Hola, mi nombre es " + name);
+var id = prompt("Tipo de documeto");
+var number = prompt("escriba numero de documento");
+console.log("Mi numero de " + id + " es " + number);
+
