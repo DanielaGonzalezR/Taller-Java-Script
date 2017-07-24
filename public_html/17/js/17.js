4af0f7626ba24a0c73a5da76727ba13e6c44ebae
DANIELA <DANIELA@10.72.133.137>
2017-07-23 22:52:10 -0500

    Finalice el punto 17.

---
+++ public_html/17/js/17.js
@@ -0,0 +1,8 @@
+//Punto 17
+
+var a = Number(prompt("Enter a number"));
+var b = Number(prompt("Enter another number"));
+var c = Number(prompt("Enter another number"));
+var rule = (b * c) / a;
+console.log("The result is: " + rule);
+
