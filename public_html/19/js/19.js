c1bcd37175a04977a512c88ddb3a1e7b3c0aa736
DANIELA <DANIELA@10.72.133.137>
2017-08-03 16:25:24 -0500

    Finalice el punto 19.

---
+++ public_html/19/js/19.js
@@ -0,0 +1,11 @@
+//Punto 19
+
+var num = Number(prompt("Enter a number"));
+
+while (num > 0) {
+  num = Number(prompt("Enter a number less than zero"));
+}
+if (num < 0) {
+  num = num * (-1);
+  console.log(num);
+}
