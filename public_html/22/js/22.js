9fb1327286a3b630d3a0120332d1ec2faa1775ea
DANIELA <DANIELA@10.72.133.137>
2017-08-03 20:57:13 -0500

    Finalice el punto 22.

---
+++ public_html/22/js/22.js
@@ -0,0 +1,14 @@
+//Punto 22
+
+var months = (12 * 15);
+console.log("There are " + months + " months in 15 years");
+var days = (365 * 15);
+console.log("There are " + days + " days in 15 years");
+var hours = (8760 * 15);
+console.log("There are " + hours + " hours in 15 years");
+var minutes = (525600 * 15);
+console.log("There are " + minutes + " minutes in 15 years");
+var seconds = (31536000 * 15);
+console.log("There are " + seconds + " seconds in 15 years");
+
+
