cb2ee5d481a2dc25f090ace84acb719e7ef21944
DANIELA <DANIELA@10.72.133.137>
2017-08-03 20:58:45 -0500

    Finalice el punto 24.

---
+++ public_html/24/js/24.js
@@ -0,0 +1,9 @@
+//Punto 24
+
+var hours = Number(prompt("Write number of hours"));
+var weks = Math.round(hours / (24 * 7));
+var result = Math.round((hours / 24));
+console.log("The result of hours, weeks is:" + weks + " and days is: " + result);
+
+
+
