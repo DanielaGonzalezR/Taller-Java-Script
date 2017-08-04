e6c1b08e98b542fa176db7ac4ca01187a2945003
DANIELA <DANIELA@10.72.133.137>
2017-08-03 20:56:38 -0500

    Finalice el punto 21.

---
+++ public_html/21/js/21.js
@@ -0,0 +1,8 @@
+//Punto 21
+
+var a = Number(prompt("Enter a number"));
+var b = Number(prompt("Enter another number"));
+
+
+var hipotenusa = ((a ** 2) + (b ** 2)) ** 0.5;
+console.log("The hipotenusa is: " + hipotenusa);
