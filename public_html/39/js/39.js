c1a4aafb4ee3965b6f87d5a24a92b4997ead8d12
DANIELA <DANIELA@10.72.133.137>
2017-08-27 21:57:37 -0500

    Finalice el punto 39.

---
+++ public_html/39/js/39.js
@@ -0,0 +1,7 @@
+//Punto 37
+
+function hello() {
+  var name = $("#name").val();
+  var names = ("Hi, I am " + name + " and I'm happy to be learning JavaScript using jQuery.");
+  $("#msj").val(names);
+}
