a23dee0b9100552e49449c4f735215e274f0ac5d
DANIELA <DANIELA@10.72.133.137>
2017-10-03 12:33:01 -0500

    Finalice el punto 11.

--- public_html/11/js/11.js
+++ public_html/11/js/11.js
@@ -1,9 +1,12 @@
+//Punto 11
 var aste = "*";
 var maximo = 6;
 var x = 1;
 var n = "";
 do {
   n = n + aste;
-  console.log(n);
-  x++;
+  if (x <= 6) {
+    console.log(n);
+    x++;
+  }
 } while (x <= maximo);
