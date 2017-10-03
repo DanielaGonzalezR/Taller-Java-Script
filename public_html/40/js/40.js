4de93206bf72ab46b102dbde8211d120de7b6676
DANIELA <DANIELA@10.72.133.137>
2017-10-03 12:24:52 -0500

    Finalice el punto 40.

--- public_html/40/js/40.js
+++ public_html/40/js/40.js
@@ -1,17 +1,13 @@
 //Point 40
 
-var enun = {
-  name: "",
-  type: "",
-  num: "",
-  gender: "",
-  height: "",
-  live: ""
-};
-
 function agen() {
-  enun.name = $("#name").val();
-
+  var name = $("#name").val();
+  var typeD = $("#type").val();
+  var numberD = $("#number").val();
+  var genderP = $("#gender").val();
+  var height = $("#height").val();
+  var live = $("#live").val();
+  var answer = "Hello, my name is " + name + ", My number of " + typeD + " is " + numberD + ", Biological gender " + genderP + ", My height is " + height + " mtrs" + live + " Is it true that I live in Carthage";
+  $("#answer").text(answer);
 }
-
-
+;
